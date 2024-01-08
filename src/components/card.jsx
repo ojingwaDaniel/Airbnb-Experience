a2af35b532602d1f55414245eb0d3f20466f446b
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-08 20:57:30 +0100

    added hero and Card Component aswhile as styling

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,36 +6,15 @@ import star from './images/Star 1.png'
 export default function Cards() {
   return (
     <div className="cards">
+      <img src={Img1} alt="" />
       <div>
-        <img src={Img1} alt="NoImage" />
-        <div className="details">
-          <img src={star} alt="" className="star" />
-          <h5>
-            5,0
-            <span>(6) USA</span>
-          </h5>
-        </div>
+        <img src={star} alt="" className="star" />
+        <span>5.0</span>
+        <span>(6) .</span>
+        <span>USA</span>
       </div>
-      <div>
-        <img src={Img2} alt="NoImage" />
-        <div className="details">
-          <img src={star} alt="" className="star" />
-          <h5>
-            5,0
-            <span>(6) USA</span>
-          </h5>
-        </div>
-      </div>
-      <div>
-        <img src={Img3} alt="NoImage" />
-        <div className="details">
-          <img src={star} alt="" className="star" />
-          <h5>
-            5,0
-            <span>(6) USA</span>
-          </h5>
-        </div>
-      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p>From $136 / person</p>
     </div>
   );
     
