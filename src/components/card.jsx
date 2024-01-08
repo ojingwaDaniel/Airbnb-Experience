0d94e868d39079dde512ce8399e793c80f386b27
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-08 20:31:06 +0100

    added hero and Card Component aswhile as styling

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,41 @@
 import Img1 from "./images/image 12.png"
 import Img2 from "./images/wedding-photography 1.png";
 import Img3 from "./images/mountain-bike 1.png";
+import star from './images/Star 1.png'
 
 export default function Cards() {
   return (
     <div className="cards">
-      <img src={Img1} alt="NoImage" />
-      <img src={Img2} alt="NoImage" />
-      <img src={Img3} alt="NoImage" />
+      <div>
+        <img src={Img1} alt="NoImage" />
+        <div className="details">
+          <img src={star} alt="" className="star" />
+          <h5>
+            5,0
+            <span>(6) USA</span>
+          </h5>
+        </div>
+      </div>
+      <div>
+        <img src={Img2} alt="NoImage" />
+        <div className="details">
+          <img src={star} alt="" className="star" />
+          <h5>
+            5,0
+            <span>(6) USA</span>
+          </h5>
+        </div>
+      </div>
+      <div>
+        <img src={Img3} alt="NoImage" />
+        <div className="details">
+          <img src={star} alt="" className="star" />
+          <h5>
+            5,0
+            <span>(6) USA</span>
+          </h5>
+        </div>
+      </div>
     </div>
   );
     
