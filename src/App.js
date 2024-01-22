bada1896bb528aec3f0ca0e98653d0a20e400a6e
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-22 23:47:17 +0100

    modified the app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import NavBar from "./components/nav";
 import Hero from "./components/hero";
 import Cards from "./components/card";
-// import Img1 from "./components/images/image 12.png";
-// import Img2 from "./components/images/wedding-photography 1.png";
-// import Img3 from "./components/images/mountain-bike 1.png";
 import data from "./data";
 function App() {
   const DataElement = data.map((item) => {
